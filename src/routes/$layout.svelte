1de2bfad60ef89ab26275fb8cbe0f78c20cb8cc9
peterfoeng <peter@peterfoeng.com>
2021-05-07 12:27:40 +1000

    Initial commit

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,43 @@
+<style>
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        max-width: 600px;
+        margin: 0 auto;
+        box-sizing: border-box;
+        text-align: center;
+    }
+
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 40px;
+    }
+
+    footer a {
+        font-weight: bold;
+    }
+
+    @media (min-width: 480px) {
+        footer {
+            padding: 40px 0;
+        }
+    }
+</style>
+
+<script lang="ts">
+    import "../app.css";
+</script>
+
+<main>
+    <slot />
+</main>
+
+<footer>
+    <p>Built with <a href="https://kit.svelte.dev" target='_blank'>sveltekit</a>. Deployed in <a href="https://kit.svelte.dev" target='_blank'>netlify</a></p>
+</footer>
